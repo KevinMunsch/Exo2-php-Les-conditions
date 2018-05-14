dd86b8c0033dd79603fd94094a6400aae2c47cf5
KevinMunsch <ke.munsch@laposte.net>
2018-05-14 16:25:46 +0200

    fini

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+  <body>
+
+<?php
+ // exercie 1
+
+$age="22";
+
+if ($age<18) {
+echo "Vous êtes mineur";
+}
+
+else {
+echo "Vous êtes majeur";
+}
+
+// exercice 2
+
+$genre="femme";
+
+if ($genre !="homme") {
+echo " Vous êtes une femme";
+}
+
+else {
+echo" Vous êtes un homme";
+}
+
+// exercice 3
+
+if ($genre !="homme") {
+echo " Vous êtes une développeuse";
+}
+
+else {
+echo " Vous êtes un developpeur";
+}
+
+// exercice 4
+
+if ($monAge >= 18){
+  echo"tu es majeur";
+}
+
+else {
+  echo"tu n'es pas majeur";
+}
+
+// exercice 5
+
+if ($maVariable == false) {
+  echo "c'est pas bon !!!";
+}
+
+else {
+  echo "c'est ok"
+}
+
+// exercice 6
+
+if ($maVariable) {
+  echo "c'est ok !!"
+}
+
+else {
+  echo "c'est pas bon!!!"
+}
+
+
+ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </body>
+
+</html>
